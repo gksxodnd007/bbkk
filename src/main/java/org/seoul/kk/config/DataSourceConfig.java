1b79b47fcb91c2f7a245ad6a6384b015068f2228
hubert.squid <hubert.squid@kakaopaycorp.com>
2018-09-01 01:35:25 +0900

    style: unused import 제거

--- src/main/java/org/seoul/kk/config/DataSourceConfig.java
+++ src/main/java/org/seoul/kk/config/DataSourceConfig.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Slf4j
 @Configuration
 public class DataSourceConfig {
