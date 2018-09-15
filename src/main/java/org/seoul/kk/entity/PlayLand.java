61398f2a565f11f9b845efafeca44484f0708545
hubert.squid <hubert.squid@kakaopaycorp.com>
2018-09-16 01:07:29 +0900

    feat: lombok @Builder를 사용하기 위한 생성자 생성

--- src/main/java/org/seoul/kk/entity/PlayLand.java
+++ src/main/java/org/seoul/kk/entity/PlayLand.java
@@ -1,13 +1,17 @@
 package org.seoul.kk.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.seoul.kk.entity.constant.Season;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Entity
 @Table(name = "TB_PLAYLAND")
