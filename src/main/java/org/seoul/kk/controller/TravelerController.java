e80fa89c4813706acb03323c68c9aabf602b01d3
onemoon <onemoonstudio@gmail.com>
2018-09-26 16:46:21 +0900

    Add : Random source

--- src/main/java/org/seoul/kk/controller/TravelerController.java
+++ src/main/java/org/seoul/kk/controller/TravelerController.java
@@ -1,53 +1,55 @@
 package org.seoul.kk.controller;
 
-import org.seoul.kk.common.model.ApiResponseModel;
-import org.seoul.kk.dto.RegisterTravelerDto;
-import org.seoul.kk.entity.Traveler;
-import org.seoul.kk.entity.constant.TravelProperty;
+import org.seoul.kk.dto.RandomNamingReturnDto;
+import org.seoul.kk.dto.RegisterNamingSourceDto;
 import org.seoul.kk.exception.BadRequestException;
 import org.seoul.kk.service.TravelerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping(value = "/v1/traveler")
 public class TravelerController {
 
+    private final TravelerService travelerService;
+
     @Autowired
-    private TravelerService travelerService;
-
-    @GetMapping(value = "/confirm")
-    public ApiResponseModel<Traveler> confirmTraveler(@RequestHeader("uuid") String uuid) {
-
-        return ApiResponseModel.<Traveler>builder()
-                .code(HttpStatus.CONFLICT.value())
-                .msg(HttpStatus.CONFLICT.getReasonPhrase())
-                .result(travelerService.getTravelerByUuid(uuid))
-                .build();
+    public TravelerController(
+            TravelerService travelerService){
+        this.travelerService = travelerService;
     }
 
-    @PostMapping(value = "/register")
-    public ApiResponseModel<Traveler> registerTraveler(@RequestHeader("uuid") String uuid,
-                                     @Valid @RequestBody RegisterTravelerDto requestBody,
-                                     BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            throw new BadRequestException();
-        }
-
-        try {
-            TravelProperty.valueOf(requestBody.getProperty());
-        } catch (IllegalArgumentException e) {
-            throw new BadRequestException("여행자 타입 enum값이 형식이 맞지 않습니다.");
-        }
-
-        return ApiResponseModel.<Traveler>builder()
-                .code(HttpStatus.OK.value())
-                .msg(HttpStatus.OK.getReasonPhrase())
-                .result(travelerService.registerTraveler(requestBody, uuid))
-                .build();
+    @GetMapping("/v1/randomnaming")
+    public @ResponseBody RandomNamingReturnDto getRandomNaming(){
+        return travelerService.randomNaming();
     }
+
+    // TODO remove this test code
+    @GetMapping("/v1/getname")
+    public @ResponseBody Object getAllNameSource(){
+
+        return travelerService.testName();
+    }
+
+    @PostMapping(value = "/v1/randomsource", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public @ResponseBody void addRandomSource(@Valid @RequestBody RegisterNamingSourceDto sourceDto,
+                                              BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new BadRequestException("Random source 필수 파라미터를 채워주세요");
+        }
+        travelerService.registerNamingSource(sourceDto);
+    }
+    // review here
+    // consume 을 명시했는데도 @RequestBody 사용하면 동작하지 않음
+
+
+
+    /* TODO
+     * Create Traveler 할때 RandomNaming 에 보내준 ReturnDTO 에 있는
+     * adjId , nounProperty 를 받아야 한다.
+     * adjId로 개체를 찾은 뒤 nounProperty를 추가해준다.
+     */
 }
